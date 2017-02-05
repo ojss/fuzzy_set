2034cd689e035ca74b36298ef747f64a1dc34527
Ojas Shirekar <ojas.shirekar@gmail.com>
2017-02-05 16:22:21 +0530

    add mul and sub methods, other minor changes

--- fuzzy_set.py
+++ fuzzy_set.py
@@ -1,6 +1,6 @@
 class FuzzySet:
-    def __init__(self, iterable: set):
-        self.f_set = iterable
+    def __init__(self, iterable: any):
+        self.f_set = set(iterable)
         self.f_list = list(iterable)
         self.f_len = len(iterable)
         for elem in self.f_set:
@@ -13,12 +13,9 @@ class FuzzySet:
         # fuzzy set union
         if len(self.f_set) != len(other.f_set):
             raise ValueError("Length of the sets is different")
-        tmp = []
         f_set = [x for x in self.f_set]
         other = [x for x in other.f_set]
-        for i in range(self.f_len):
-            tmp.append(f_set[i] if f_set[i][1] > other[i][1] else other[i])
-        return FuzzySet(tmp)
+        return FuzzySet([f_set[i] if f_set[i][1] > other[i][1] else other[i] for i in range(len(self))])
 
     def __and__(self, other):
         # fuzzy set intersection
@@ -27,9 +24,8 @@ class FuzzySet:
         tmp = []
         f_set = [x for x in self.f_set]
         other = [x for x in other.f_set]
-        for i in range(self.f_len):
-            tmp.append(f_set[i] if f_set[i][1] < other[i][1] else other[i])
-        return FuzzySet(tmp)
+
+        return FuzzySet([f_set[i] if f_set[i][1] < other[i][1] else other[i] for i in range(len(self))])
 
     def __invert__(self):
         f_set = [x for x in self.f_set]
@@ -38,10 +34,18 @@ class FuzzySet:
         return FuzzySet(f_set)
 
     def __sub__(self, other):
-        pass
+        if len(self) != len(other):
+            raise ValueError("Length of the sets is different")
+        return self & ~other
+
+    def __mul__(self, other):
+        if len(self) != len(other):
+            raise ValueError("Length of the sets is different")
+        return FuzzySet([(self[i][0], self[i][1] * other[i][1]) for i in range(len(self))])
 
     def __len__(self):
-        return sum([1 for i in self.f_set])
+        self.f_len = sum([1 for i in self.f_set])
+        return self.f_len
 
     def __str__(self):
         return f'{[x for x in self.f_set]}'
@@ -55,8 +59,10 @@ class FuzzySet:
 
 a = FuzzySet({('x', 0.2), ('y', 0.8)})
 b = FuzzySet({('x', 0.3), ('y', 0.3)})
+print(f'a -> {a}')
+print(f'b -> {b}')
 print(f'Fuzzy union: \n{a | b}')
-
 print(f'Fuzzy intersection: \n{a & b}')
-
-print(f'Fuzzy inversion: \n{~a}')
+print(f'Fuzzy inversion of b: \n{~b}')
+print(f'Fuzzy Subtraction: \n{a - b}')
+print(f'Multiplication: \n{a * b}')
