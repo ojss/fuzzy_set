49fb58569b1521f158313b54c55dda7a21e1593d
Ojas Shirekar <ojas.shirekar@gmail.com>
2017-02-06 21:08:23 +0530

    add max_min, cartesian product and changed iter

--- fuzzy_set.py
+++ fuzzy_set.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class FuzzySet:
     def __init__(self, iterable: any):
         self.f_set = set(iterable)
@@ -21,7 +24,6 @@ class FuzzySet:
         # fuzzy set intersection
         if len(self.f_set) != len(other.f_set):
             raise ValueError("Length of the sets is different")
-        tmp = []
         f_set = [x for x in self.f_set]
         other = [x for x in other.f_set]
 
@@ -43,6 +45,31 @@ class FuzzySet:
             raise ValueError("Length of the sets is different")
         return FuzzySet([(self[i][0], self[i][1] * other[i][1]) for i in range(len(self))])
 
+    def __mod__(self, other):
+        # cartesian product
+        print(f'The size of the relation will be: {len(self)}x{len(other)} ')
+        mx = self
+        mi = other
+        tmp = [[] for i in range(len(mx))]
+        i = 0
+        for x in mx:
+            for y in mi:
+                tmp[i].append(min(x[1], y[1]))
+            i += 1
+        return np.array(tmp)
+
+    @staticmethod
+    def max_min(array1: np.ndarray, array2: np.ndarray):
+        tmp = np.zeros((array1.shape[0], array2.shape[1]))
+        t = list()
+        for i in range(len(array1)):
+            for j in range(len(array2[0])):
+                for k in range(len(array2)):
+                    t.append(round(min(array1[i][k], array2[k][j]), 2))
+                tmp[i][j] = max(t)
+                t.clear()
+        return tmp
+
     def __len__(self):
         self.f_len = sum([1 for i in self.f_set])
         return self.f_len
@@ -54,11 +81,15 @@ class FuzzySet:
         return self.f_list[item]
 
     def __iter__(self):
-        return self.f_list
+        for i in range(len(self)):
+            yield self[i]
 
 
 a = FuzzySet({('x', 0.2), ('y', 0.8)})
 b = FuzzySet({('x', 0.3), ('y', 0.3)})
+c = FuzzySet({('x', 0.3), ('y', 0.3), ('z', 0.5)})
+x = FuzzySet({('a', 0.5), ('b', 0.3), ('c', 0.7)})
+y = FuzzySet({('a', 0.6), ('b', 0.4)})
 print(f'a -> {a}')
 print(f'b -> {b}')
 print(f'Fuzzy union: \n{a | b}')
@@ -66,3 +97,8 @@ print(f'Fuzzy intersection: \n{a & b}')
 print(f'Fuzzy inversion of b: \n{~b}')
 print(f'Fuzzy Subtraction: \n{a - b}')
 print(f'Multiplication: \n{a * b}')
+r = np.array([[0.5, 0.1], [0.2, 0.9], [0.8, 0.6]])
+s = np.array([[0.6, 0.4, 0.7], [0.5, 0.8, 0.9]])
+print(f"Max Min: of \n{r} \nand \n{s}\n:\n\n")
+
+print(FuzzySet.max_min(r, s))
